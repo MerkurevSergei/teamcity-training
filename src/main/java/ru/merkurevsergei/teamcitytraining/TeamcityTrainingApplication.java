c209875f7fee66b0281332f2a4904f9b0bed3529
MerkurevSergei <MerkurevSergei@yandex.ru>
2022-09-17 13:09:37 +0300

    Init project

---
+++ src/main/java/ru/merkurevsergei/teamcitytraining/TeamcityTrainingApplication.java
@@ -0,0 +1,13 @@
+package ru.merkurevsergei.teamcitytraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcityTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/merkurevsergei/teamcitytraining/TeamcityTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkurevsergei.teamcitytraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
